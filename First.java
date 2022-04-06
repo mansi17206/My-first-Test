dbeaf8b1a1f85a02808eeab59c8527421ff0f177
Mansi <mansi_17206@yahoo.com>
2022-04-06 19:37:58 +0100

    First comment

---
+++ First.java
@@ -0,0 +1,18 @@
+package com.learning.java.myfristpracticetest;
+
+public class First {
+    public static void main(String[] args) {
+   System.out.println("Automation Testing");
+   System.out.println("Advantages of Automation testing");
+   System.out.println("Disadvantages of Automation testing");
+   System.out.println("Java features");
+   System.out.println("Java Architecture");
+   System.out.println("Java variables");
+
+
+
+
+    }
+
+
+}
